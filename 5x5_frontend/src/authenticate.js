6df486281599b738ed4a5b100f506991374e27e8
cgillam <62260365+cgillam@users.noreply.github.com>
2020-08-26 20:14:39 -0600

    sign in button fixed

--- 5x5_frontend/src/authenticate.js
+++ 5x5_frontend/src/authenticate.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useRef } from "react"
 import UserContext from './user.js'
 import { Button, TextField, Paper, Box, Grid } from '@material-ui/core'
 
@@ -21,23 +21,28 @@ export default function Authenticate() {
         >Log Out</Button>
     );
 
+    const formRef = useRef(null)
+    const submitForm = (login) => {
+        const data = new FormData(formRef.current);
+        fetch(`/api/user/${login ? "login" : "signup"}`, {
+            method: 'post',
+            body: data,
+            credentials: "include"
+        }).then(res => console.log(res) || res.json())
+            .then(newUser => {
+                user.setUser(newUser)
+            })
+
+    }
 
     return (
         <Grid container justify="center" style={{ minHeight: '75vh' }}>
             <Paper style={{ margin: 'auto', padding: '0.5em', backgroundColor: 'black' }} elevation={5} >
                 <form
+                    ref={formRef}
                     onSubmit={(e) => {
                         e.preventDefault()
-                        const data = new FormData(e.target)
-                        fetch(`/api/user/${e.nativeEvent.submitter.value}`, {
-                            method: 'post',
-                            body: data,
-                            credentials: "include"
-                        }).then(res => console.log(res) || res.json())
-                            .then(newUser => {
-                                user.setUser(newUser)
-                            })
-
+                        submitForm(true);
                     }}
                     style={{
                         display: 'grid',
@@ -47,7 +52,7 @@ export default function Authenticate() {
                     <TextField style={{ color: 'white' }} classes={{ root: 'white-input' }} variant="outlined" color="primary" label="Username" name="userName" />
                     <TextField style={{ color: 'white' }} classes={{ root: 'white-input' }} variant="outlined" color="primary" type="password" label="Password" name="password" />
                     <Button variant="outlined" color="primary" style={{ color: 'white' }} classes={{ label: 'left-label' }} type="submit" name="action" value="login">Login</Button>
-                    <Button variant="outlined" color="primary" style={{ color: 'white' }} classes={{ label: 'left-label' }} name="action" value="signup">Sign Up</Button>
+                    <Button onClick={() => submitForm(false)} variant="outlined" color="primary" style={{ color: 'white' }} classes={{ label: 'left-label' }} name="action" value="signup">Sign Up</Button>
                 </form >
             </Paper>
         </Grid >
